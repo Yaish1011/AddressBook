4a6c8421c418bdc6d9877e2f042c43b8a4c3882f
Yaish1011 <ymakode4@gmail.com>
2024-11-21 14:16:57 +0530

    UC2: Ability to Add a new Contact

---
+++ src/com/AddressBook/AddressBook.java
@@ -0,0 +1,12 @@
+package com.AddressBook;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    public ArrayList<Contact> contactList = new ArrayList<>();
+    public  ArrayList<Contact> contactList(Contact d)
+    {
+        contactList.add(d);
+        return contactList;
+    }
+}
